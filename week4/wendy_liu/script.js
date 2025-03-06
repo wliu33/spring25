f2696bac2a13eabf64b4f73fa9cfdabfc1d68280
wliu33 <wendyliu033@berkeley.edu>
2025-03-06 03:50:48 -0800

    Updated my cookie clicker!
    Tried to add a clicks per Second (kinda buggy still)

--- week4/wendy_liu/script.js
+++ week4/wendy_liu/script.js
@@ -1,3 +1,49 @@
+//must be kept static
+let clicks = 0;
+let cps = 0;
+let ms = 0;
+let started = false;
+setInterval(() => {
+    if(started) {
+        ms+=10; //add 10 millisec
+        //calculate the cps
+        cps = Math.round(clicks / (ms/1000));
+        //call update to update cps on the screen
+        update();
+    }
+}, 10)
+
 function addOneToCounter(){
-    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) + 1
+    let counterElem = document.getElementById("counter");
+    let totalCount = parseInt(document.getElementById("counter").innerText) + 1;
+    counterElem.innerText = totalCount;
+    let currentCookie;
+    //initialize cps to 0 until user made first click
+
+    //munch sound
+    let audio = document.getElementById("crunch-sound");
+    audio.play();
+
+    clicks++;
+    currentCookie = document.getElementById("oreo.img");
+    //change the image on even clicks only
+    if (totalCount % 2 === 0) {
+        currentCookie.src = "bitten_cookie.jpg"; 
+
+    } else {
+        currentCookie.src = "full_oreo.png";  
+    }
+    currentCookie.style.width = "40%";
+    currentCookie.style.height = "40%";
+    currentCookie.style.alignSelf = "center";
+   
+    //starts time only once (if haven't clicked yet)
+    if (!started) {
+        started = true;      
+    }
+        
+}
+
+function update() {
+    document.getElementById("cps").innerText = "Clicks per Second: " + cps;
 }
